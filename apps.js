321317b97bc8dee4ce0f48fa78405d109d8b3e2e
Lucas Solari <128732015+Lucaso1992@users.noreply.github.com>
2023-04-29 10:18:42 +0000

    ejercicios js

---
+++ apps.js
@@ -0,0 +1,26 @@
+const prompt = require ("prompt-sync")({sigint: true});
+
+const welcomeMessage = prompt("Hola, elige una aplicacion:
+                                1. Contar letras
+                                2. Generar contraseña
+                                3. Calculadora
+                                4. Encontrar palabras
+                                5. Generar numero aleatorio
+                                6. Piedra, papel, tijera
+                                7.Blackjack");
+    
+ if
+
+const countLetters = "countLetters";
+
+const generatePass = "generatePass";
+
+const calculator = "calculator";
+
+const findWords = "findWords";
+
+const generateRandomNumbers = "generateRandomNumbers";
+
+const rockPaperScissors = "rockPaperScissors";
+
+const blackjack = "blackjack";
---
+++ test.js
@@ -0,0 +1,4 @@
+const prompt = require ("prompt-sync") ({sigint: true});
+
+const name = prompt ("¿Como te llamas?");
+console.log()
