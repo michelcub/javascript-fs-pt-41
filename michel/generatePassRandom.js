0cca0e1ff7410d696048e208f71bc47b941b020e
Michel <49735520+michelcub@users.noreply.github.com>
2023-05-02 17:17:11 +0000

    add change

--- michel/generatePassRandom.js
+++ michel/generatePassRandom.js
@@ -1,11 +1,12 @@
 const prompt = require('prompt-sync')({ sigint: true});
 
-const paswordLength = prompt('Escribe el largo deseado de la contrasena : ');
+const paswordLength = parseInt(prompt('Escribe el largo deseado de la contrasena : '));
+
 function generateRandomPass(passwordLength) {
     const charset = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()_+";
     let password = " ";
     for (let i = 0; i < passwordLength; i++){
-        let randomIndex = Math.floor(Math.random() * charset.length);
+        const randomIndex = Math.floor(Math.random() * charset.length);
         password += charset[randomIndex];
     }
     return password;
