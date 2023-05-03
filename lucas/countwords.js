d8e94d9c5a1ae36724fa963a1e93a1b000c63bf6
Lucas Solari <128732015+Lucaso1992@users.noreply.github.com>
2023-05-03 09:02:29 +0000

    count words

---
+++ lucas/countwords.js
@@ -0,0 +1,9 @@
+
+
+const myString = "Este es el texto del cual debo contar la cantidad de palabras";
+
+const words = myString.split(" ");
+
+const wordsLength = words.length;
+
+console.log(wordsLength)
