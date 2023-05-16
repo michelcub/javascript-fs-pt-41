a0dc4bc5a99423a152b2055431ffd4b5d586da5c
Alvaro Martin Fernandez <91843474+AlvaroMartinFernandez@users.noreply.github.com>
2023-05-16 18:01:41 +0000

    Emoji Card Finish

--- apps/memory/components/board.js
+++ apps/memory/components/board.js
@@ -1,6 +1,14 @@
+import card from "./card.js";
+import { emojis } from "../utils/constants/emoji.js";
+
+
 const board = () => {
     const newBoard = document.createElement("div");
     newBoard.className = "div-board";
+    emojis.forEach(element => {
+        newBoard.append(card(element));   
+    });
+    
     return newBoard;
 }
 export default board;
