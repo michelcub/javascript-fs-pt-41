21732b8fa33ae2f083ed5d78788a028e7b0af6a0
Lucas Solari <128732015+Lucaso1992@users.noreply.github.com>
2023-05-16 16:35:21 +0000

    modificacion pull request

--- test.js
+++ test.js
@@ -1,8 +1,4 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
 const name = prompt('¿Cómo te llamas?');
-<<<<<<< HEAD
 console.log(`Hola, ${name}`)
-=======
-console.log(`Hola, ${name}`)
->>>>>>> e7ca51c9e2e6496461b5bbe1b8bdcc99daca32a7
