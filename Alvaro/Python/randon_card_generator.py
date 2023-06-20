0c5003fa3381a4267a6c32732ce0bb1ac4d53566
Alvaro Martin Fernandez <91843474+AlvaroMartinFernandez@users.noreply.github.com>
2023-06-20 19:32:51 +0000

    Python card_ramdon

--- Alvaro/Python/randon_card_generator.py
+++ Alvaro/Python/randon_card_generator.py
@@ -7,10 +7,24 @@ desk = {
     "diamont": ["A","2","3","4","5","6","7","8","9","J","Q","K"]
 
 }
+is_runnig = True
 
-keys = list(desk)
-select_key=random.choice(keys)
-select_card = random.choice(desk[select_key])
-desk[select_key].remove(select_card)
-print(f'La carta seria el {select_card} de {select_key}')
-print(desk)
+
+def select():
+    global is_runnig
+    keys = list(desk)
+    select_key=random.choice(keys)
+
+    if not len(select_key):
+        desk.pop(select_key)
+    else:
+        select_card = random.choice(desk[select_key])
+        desk[select_key].remove(select_card)
+        print(f'La carta seria el {select_card} de {select_key}')
+        print(desk)
+
+    if not len(keys):
+        is_runnig = False
+
+while is_runnig:
+    select()
