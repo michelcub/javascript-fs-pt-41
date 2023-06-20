8f7991fdc8546fea41623c01524eefcb77fa3781
Dámaso Armas <127236212+dasktorm@users.noreply.github.com>
2023-06-20 18:55:42 +0000

    domain-gen

--- damaso/domain-gen.py
+++ damaso/domain-gen.py
@@ -2,12 +2,8 @@ pronoun = ['the', 'our']
 adj = ['great', 'big']
 noun = ['jogger', 'racoon']
 
-domains = []
-
 for p in pronoun:
     for a in adj:
         for n in noun:
             domain = p + a + n + '.com'
-            domains.append(domain)
-
-print(domains)
+            print(domain)
